a216570ac95c83fd561625b0ad06158b92d8987b
Denis Pianelli <denis.pianelli@gmail.com>
2024-09-04 11:06:14 +0200

    feat: add Skills component to the Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import Intro from '@/components/intro';
 import RecentPosts from '@/components/recent-posts';
 import RecentProjects from '@/components/recent-projects';
+import Skills from '@/components/skills';
 
 /**
  * Renders the Home component.
@@ -11,6 +12,7 @@ export default function Home() {
   return (
     <section className="container max-w-3xl py-20">
       <Intro />
+      <Skills />
       <RecentPosts />
       <RecentProjects />
     </section>
