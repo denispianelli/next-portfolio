5aeb93dfbbb804816228e9e5514debeb846abc8b
Denis Pianelli <denis.pianelli@gmail.com>
2024-09-05 10:55:15 +0200

    feat: add badge to ContactPage for better visual representation

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,4 +1,5 @@
 import ContactForm from '@/components/contact-form';
+import { Badge } from '@/components/ui/badge';
 
 /**
  * Renders the ContactPage component.
@@ -8,7 +9,12 @@ import ContactForm from '@/components/contact-form';
 export default function ContactPage() {
   return (
     <section className="container max-w-3xl pb-24 pt-20">
-      <h2 className="title">Let's talk about your project</h2>
+      <div className="mb-2 flex justify-center">
+        <Badge className="text-sm">Contact</Badge>
+      </div>
+      <h2 className="title mb-12 text-center text-4xl font-semibold no-underline">
+        Let's get in touch
+      </h2>
       <ContactForm />
     </section>
   );
