ff3bc133783b01da43d06f3ec1fb05a213c755b6
Denis Pianelli <denis.pianelli@gmail.com>
2024-09-05 10:55:25 +0200

    chore: update intro component to use larger title font size

--- src/components/intro.tsx
+++ src/components/intro.tsx
@@ -5,7 +5,7 @@ export default function Intro() {
   return (
     <section className="flex flex-col-reverse items-start gap-x-10 gap-y-4 pb-24 md:flex-row md:items-center">
       <div className="mt-2 flex-1 md:mt-0">
-        <h1 className="title no-underline">Hey, I&#39;m Denis.</h1>
+        <h1 className="title text-[3rem] no-underline">Hey, I&#39;m Denis.</h1>
         <p className="mt-3 font-light text-muted-foreground">
           I&#39;m a web developer based in Plaisir, France. I&#39;m passionate
           about exploring new technologies and creating innovative projects.
